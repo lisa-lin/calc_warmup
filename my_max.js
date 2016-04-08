c4d7ee8238a5486d1c5100a5ddf03a356e0009bc
Lisa Lin <lin.lisa@hotmail.com>
2016-04-08 13:08:25 -0400

    Refactor code

--- my_max.js
+++ my_max.js
@@ -3,13 +3,13 @@
 function my_max() {
   var max = 0;
   var i;
-  var array=[];
+  var array = [];
 
-  for(i=0; i<5; i++){
-    var number=prompt("Enter 5 numbers to find the max");
-    number=parseFloat(number);
-    if(number>max)
-     max=number;
+  for(i = 0; i < 5; i++){
+    var number = prompt("Enter 5 numbers to find the max");
+    number = parseFloat(number);
+    if(number > max)
+     max = number;
      // if you need to hold the value
      array.push(number);
   }
--- vowel_count.js
+++ vowel_count.js
@@ -3,15 +3,15 @@
 function vowel_count() {
   a = "holy guacamole!";
 
-  var syl = 0;
+  var numVowels = 0;
 
-  for(var i=0; i<a.length; i++) {
+  for(var i = 0; i < a.length; i++) {
           if('aouiey'.search(a[i]) > -1){
-              syl++;
+              numVowels++;
           }
   }
 
-  alert(syl+" vowels")
+  alert(numVowels + " vowels")
 }
 
 vowel_count();
